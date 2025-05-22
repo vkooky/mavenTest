feb69093cedab390decb4303b46f6d4aa9f17be4
vkooky <1814739863@qq.com>
2025-05-22 10:43:26 +0800

    第一次提交

---
+++ src/test/java/com/api/login/TestLogin.java
@@ -0,0 +1,55 @@
+package com.api.login;
+
+import org.testng.annotations.*;
+
+public class TestLogin {
+//    public static void main(String[] args) {
+//        System.out.println("111111111111");
+//    }
+
+
+//    @Test(enabled = false)
+//    public void testlogin(){
+//        System.out.println("测试登录用例");
+//    }
+
+    @BeforeSuite
+    public void conn1(){
+        System.out.println("测试套件前置，相当于一个套件");
+    }
+
+    @BeforeTest
+    public void conn2(){
+        System.out.println("测试模块前置，相当于一个模块");
+    }
+
+    @BeforeClass
+    public void conn3(){
+        System.out.println("测试类前置，相当于一个子模块");
+    }
+
+    @BeforeMethod
+    public void conn4(){
+        System.out.println("测试用例前置，相当于一个功能");
+    }
+
+    @Test(priority =-1,invocationCount = 2,dependsOnMethods = {"testlogin"})
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test(dependsOnMethods = {"test3"})
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("test3");
+    }
+
+    @Test
+    public void testlogin(){
+        System.out.println("测试登录用例");
+    }
+}
